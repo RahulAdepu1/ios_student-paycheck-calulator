f465083c251e7f53dcca18d6347b85a94b6db977
RahulAdepu1 <67685274+RahulAdepu1@users.noreply.github.com>
2023-04-07 13:25:23 -0500

    Initcial Commit

---
+++ studPaycheckCal/studPaycheckCal/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  studPaycheckCal
+//
+//  Created by Rahul Adepu on 4/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+//MARK: - Preview
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ studPaycheckCal/studPaycheckCal/studPaycheckCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  studPaycheckCalApp.swift
+//  studPaycheckCal
+//
+//  Created by Rahul Adepu on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct studPaycheckCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
