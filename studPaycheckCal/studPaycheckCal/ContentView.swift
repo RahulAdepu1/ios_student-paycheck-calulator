2a9e229e41a775a32e7e1c2ad631282046ce2166
RahulAdepu1 <67685274+RahulAdepu1@users.noreply.github.com>
2023-04-08 12:11:09 -0500

    created mainViewModel to handle the logic and selfCheckViewResult to display the results

--- studPaycheckCal/studPaycheckCal/ContentView.swift
+++ studPaycheckCal/studPaycheckCal/ContentView.swift
@@ -7,75 +7,75 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var selectedCountry = "USA"
-    @State private var selectedCity = "New York"
-    @State private var isShowingAlert = false
+struct NumberSelectionView: View {
+    @StateObject var viewModel = NumberSelectionViewModel()
     
     var body: some View {
-        NavigationView {
-            VStack {
-                Picker("Country", selection: $selectedCountry) {
-                    Text("USA").tag("USA")
-                    Text("Canada").tag("Canada")
-                    Text("Mexico").tag("Mexico")
+        VStack {
+            Picker("Number 1", selection: $viewModel.number1) {
+                ForEach(1...100, id: \.self) { number in
+                    Text("\(number)").tag(number)
                 }
-                .pickerStyle(.wheel)
-                
-                Picker("City", selection: $selectedCity) {
-                    switch selectedCountry {
-                    case "USA":
-                        Text("New York").tag("New York")
-                        Text("Los Angeles").tag("Los Angeles")
-                        Text("Chicago").tag("Chicago")
-                    case "Canada":
-                        Text("Toronto").tag("Toronto")
-                        Text("Montreal").tag("Montreal")
-                        Text("Vancouver").tag("Vancouver")
-                    case "Mexico":
-                        Text("Mexico City").tag("Mexico City")
-                        Text("Cancun").tag("Cancun")
-                        Text("Guadalajara").tag("Guadalajara")
-                    default:
-                        Text("Error")
-                    }
-                }
-                .pickerStyle(.wheel)
-                
-                NavigationLink(
-                    destination: SecondView(),
-                    label: {
-                        Text("Go to Next View")
-                    })
-                    .disabled(selectedCountry == "USA" && selectedCity == "New York")
-                    .onChange(of: selectedCountry, perform: { _ in
-                        isShowingAlert = true
-                    })
-                    .onChange(of: selectedCity, perform: { _ in
-                        isShowingAlert = true
-                    })
-                    .alert(isPresented: $isShowingAlert) {
-                        Alert(
-                            title: Text("Picker values have not changed"),
-                            message: Text("Please select a different value for the pickers to proceed."),
-                            dismissButton: .default(Text("OK"))
-                        )
-                    }
             }
-            .navigationTitle("Pickers and Navigation")
+            .pickerStyle(WheelPickerStyle())
+            
+            Picker("Number 2", selection: $viewModel.number2) {
+                ForEach(1...100, id: \.self) { number in
+                    Text("\(number)").tag(number)
+                }
+            }
+            .pickerStyle(WheelPickerStyle())
+            
+            Button("Multiply") {
+                viewModel.multiplyNumbers()
+            }
+            .disabled(!viewModel.canNavigateToNextView)
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            .padding(.top, 20)
+            
+            NavigationLink(
+                destination: ResultView(result: viewModel.result),
+                isActive: $viewModel.navigateToNextView) {
+                EmptyView()
+            }
         }
+        .navigationTitle("Number Selection")
     }
 }
 
-struct SecondView: View {
+struct ResultView: View {
+    let result: Int
+    
     var body: some View {
-        Text("This is the Second View")
-            .navigationTitle("Second View")
+        Text("Result: \(result)")
+            .font(.title)
+            .padding()
+    }
+}
+
+class NumberSelectionViewModel: ObservableObject {
+    @Published var number1 = 1
+    @Published var number2 = 1
+    @Published var result = 1
+    @Published var navigateToNextView = false
+    
+    var canNavigateToNextView: Bool {
+        return number1 != 1 && number2 != 1
+    }
+    
+    func multiplyNumbers() {
+        result = number1 * number2
+        navigateToNextView = true
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            NumberSelectionView()
+        }
     }
 }
---
+++ studPaycheckCal/studPaycheckCal/MainViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewModel.swift
+//  studPaycheckCal
+//
+//  Created by Rahul Adepu on 4/8/23.
+//
+
+import Foundation
