278f5723bd4351d4ad1af8eaeab3ec25b7c54382
RahulAdepu1 <67685274+RahulAdepu1@users.noreply.github.com>
2023-04-23 20:43:19 -0500

    created custom TabBar View

---
+++ studPaycheckCal/studPaycheckCal/Custom/CustomTabBarView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTabBarView.swift
+//  studPaycheckCal
+//
+//  Created by Rahul Adepu on 4/23/23.
+//
+
+import SwiftUI
+
+struct CustomTabBarView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct CustomTabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTabBarView()
+    }
+}
