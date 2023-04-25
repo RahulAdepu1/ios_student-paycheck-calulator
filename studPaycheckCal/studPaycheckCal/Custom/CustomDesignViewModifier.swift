00b7b5d6efe9bae947cefb59288cf70cb9ba9ab7
RahulAdepu1 <67685274+RahulAdepu1@users.noreply.github.com>
2023-04-25 00:14:41 -0500

    added extra custom design ViewModifiers

--- studPaycheckCal/studPaycheckCal/Custom/CustomDesignViewModifier.swift
+++ studPaycheckCal/studPaycheckCal/Custom/CustomDesignViewModifier.swift
@@ -7,10 +7,35 @@
 
 import SwiftUI
 
-struct CustomTextDesign: ViewModifier {
+struct CustomTextDesign1: ViewModifier {
     func body(content: Content) -> some View {
         content
             .frame(width: 120, height: 50)
+            .background(Color.white)
+            .cornerRadius(15)
+            .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 2)
+    }
+}
+
+struct CustomTextDesign2: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.caption)
+            .multilineTextAlignment(.center)
+            .padding(5)
+            .frame(maxWidth: .infinity)
+            .background(Color.white)
+            .cornerRadius(15)
+            .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 2)
+    }
+}
+
+struct CustomTextDesign3: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.caption)
+            .multilineTextAlignment(.center)
+            .frame(maxWidth: .infinity)
             .background(Color.white)
     }
 }
@@ -44,6 +69,17 @@ struct CustomBlockDesign: ViewModifier {
     func body(content: Content) -> some View {
         content
             .padding(10)
+            .background(Color.white)
+            .cornerRadius(15)
+            .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 2)
+    }
+}
+
+struct CustomBlockDesign2: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(10)
+            .frame(maxWidth: .infinity)
             .background(Color.white)
             .cornerRadius(15)
             .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 2)
